21fce510caedaa717c6338c4354b885b3a86a123
Chris Casson <cassonchris723@gmail.com>
2018-01-26 17:40:26 -0600

    Add paypal checkout button.

--- PayPalIntegration/Controllers/HomeController.cs
+++ PayPalIntegration/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace PayPalIntegration.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult PayPal()
+        {
+            return View();
+        }
     }
 }
